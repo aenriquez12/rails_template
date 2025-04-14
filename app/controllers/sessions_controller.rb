a7040c2c07ef995bb59cc5d4cd8180bfbd21245f
aenriquez12 <156951230+aenriquez12@users.noreply.github.com>
2025-04-14 06:18:50 +0000

    log in server 500

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,14 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access only: %i[ new create ]
-  def new
-  end
-
   def create
-    user = User.find_by(email_address: params[:email_address])
+    user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to dashboard_path, notice: "Signed in successfully."
+      redirect_to root_path, notice: "Logged in!"
     else
-      flash[:alert] = "Invalid email or password."
-      render :new
+      flash.now[:alert] = "Invalid credentials"
+      render :new, status: :unprocessable_entity
     end
   end
-
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Signed out successfully."
